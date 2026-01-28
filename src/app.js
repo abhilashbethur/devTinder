38032a9e9e25d0aad9667c18050a8d6a692cb680
abhilashbethur <abhilashbethur@gmail.com>
2026-01-28 08:32:46 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -6,6 +6,16 @@ app.get("/user/:userId/:username/", (req, res) => {
   res.send({ msg: req.params });
 });
 
+app.get("/middle", (req, res, next) => {
+  console.log("1st");
+  next();
+});
+
+app.get("/middle", (req, res, next) => {
+  console.log("2nd");
+  res.send("hallelujah");
+});
+
 app.use("/", (req, res) => {
   res.send("Bandya ");
 });
